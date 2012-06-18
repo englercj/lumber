e1078f25c4cf33478d8479bdf283206568c3a19e
Chad Engler <englercj@live.com>
2012-06-17 21:48:05 -0400

    Added more tests, fixed bug

--- test/lumber/transports/console-test.js
+++ test/lumber/transports/console-test.js
@@ -18,13 +18,29 @@ vows.describe('Console').addBatch({
         topic: function() {
             return new lumber.transports.Console();
         },
-        'has the correct defaults': function(con) {
-            assert.instanceOf(con.encoder, lumber.encoders.Text);
-            assert.isFunction(con.encoder.encode);
-            assert.equal(con.level, 'info');
-        },
-        'has the correct functions': function(con) {
-            assert.isFunction(con.log);
-        }
+	'has': {
+            'the correct defaults': function(con) {
+		assert.instanceOf(con.encoder, lumber.encoders.Text);
+		assert.isFunction(con.encoder.encode);
+		assert.equal(con.level, 'info');
+            },
+            'the correct functions': function(con) {
+		assert.isFunction(con.log);
+            }
+	},
+	'should': {
+	    topic: function(con) {
+		process.stdout.on('data', this.callback);
+
+		var logger = new lumber.Logger({ transports: [con] });
+		logger.info('Some info data', this.callback);
+	    },
+	    'write to the proper stream': function(data) {
+		assert.isTrue(true);
+	    },
+	    'write properly enocoded data': function(data) {
+		assert.equal(data, 'info: Some info data');
+	    }
+	}
     }
 }).export(module);
